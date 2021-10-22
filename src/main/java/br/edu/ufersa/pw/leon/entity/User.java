8b39a48d594b89833ea2516f936f3b93d431aef8
Ascênio Sanderson <ascenio@hotmail.com>
2021-10-21 23:10:36 -0300

    refactor: User

--- src/main/java/br/edu/ufersa/pw/leon/entity/User.java
+++ src/main/java/br/edu/ufersa/pw/leon/entity/User.java
@@ -1,118 +1,67 @@
 package br.edu.ufersa.pw.leon.entity;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.UUID;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
     @Column(unique = true)
     private String email;
-    private String password;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private UUID uuid;
-    private String name;
     private LocalDate birthday;
+    private String password;
     private String cellphone;
-    private String avatar_url;
-    private int available_reschedules;
-    private int available_experiments;
+    private String avatarURL;
+    private int availableReschedules;
+    private int availableExperiments;
 
-    public User() {
-        uuid = UUID.randomUUID();
-    }
-    
-
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
+    public User(String name, String email, String password) {
+        this.name = name;
         this.email = email;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
+    public User() {
+
+    }
+
     public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UUID getUuid() {
-        return this.uuid;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
+        return id;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getEmail() {
+        return email;
     }
 
     public LocalDate getBirthday() {
-        return this.birthday;
+        return birthday;
     }
 
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
+    public String getPassword() {
+        return password;
     }
 
     public String getCellphone() {
-        return this.cellphone;
+        return cellphone;
     }
 
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
+    public String getAvatarURL() {
+        return avatarURL;
     }
 
-    public String getAvatar_url() {
-        return this.avatar_url;
+    public int getAvailableReschedules() {
+        return availableReschedules;
     }
 
-    public void setAvatar_url(String avatar_url) {
-        this.avatar_url = avatar_url;
+    public int getAvailableExperiments() {
+        return availableExperiments;
     }
-
-    public int getAvailable_reschedules() {
-        return this.available_reschedules;
-    }
-
-    public void setAvailable_reschedules(int available_reschedules) {
-        this.available_reschedules = available_reschedules;
-    }
-
-    public int getAvailable_experiments() {
-        return this.available_experiments;
-    }
-
-    public void setAvailable_experiments(int available_experiments) {
-        this.available_experiments = available_experiments;
-    }
-    
-
-    
 }
