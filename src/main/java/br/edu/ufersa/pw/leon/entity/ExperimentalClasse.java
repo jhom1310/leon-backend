31799735bdcde1bbae229f5a6dd2854fbb661c99
Ascênio Sanderson <ascenio@hotmail.com>
2021-10-21 23:24:07 -0300

    refactor: ExperimentalClasse

---
+++ src/main/java/br/edu/ufersa/pw/leon/entity/ExperimentalClasse.java
@@ -0,0 +1,23 @@
+package br.edu.ufersa.pw.leon.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class ExperimentalClasse {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public ExperimentalClasse(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
