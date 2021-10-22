13878d39ddf97f98e1b2932015fac33217338753
Ascênio Sanderson <ascenio@hotmail.com>
2021-10-21 23:12:02 -0300

    refactor: Admin

--- src/main/java/br/edu/ufersa/pw/leon/entity/Admin.java
+++ src/main/java/br/edu/ufersa/pw/leon/entity/Admin.java
@@ -1,55 +1,44 @@
 package br.edu.ufersa.pw.leon.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table(name = "tb_admin")
+@Table(name = "admins")
 public class Admin {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
     @Column(unique = true)
     private String email;
     private String password;
     private String name;
-   
+    @OneToMany(mappedBy = "admin")
+    private List<Post> posts;
 
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
+    public Admin(String email, String password, String name) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
 }
