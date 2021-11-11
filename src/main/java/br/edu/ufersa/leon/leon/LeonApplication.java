3070e0d3f9011aaf1a482e78e26869f68dd3e20a
Ascênio Sanderson <ascenio@hotmail.com>
2021-11-11 20:47:13 -0300

    refactor: reorganiza entry point da aplicação

--- src/main/java/br/edu/ufersa/leon/leon/LeonApplication.java
+++ src/main/java/br/edu/ufersa/leon/leon/LeonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeonApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LeonApplication.class, args);
     }
