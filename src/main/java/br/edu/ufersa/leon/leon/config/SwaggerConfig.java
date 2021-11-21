c634f86abd665b63ff4c5a1bd0e45ca0ae80aace
Ascênio Sanderson <ascenio@hotmail.com>
2021-11-21 12:32:46 -0300

    fix: exibe no Swagger apenas controllers do projeto

--- src/main/java/br/edu/ufersa/leon/leon/config/SwaggerConfig.java
+++ src/main/java/br/edu/ufersa/leon/leon/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package br.edu.ufersa.leon.leon.config;
 
+import br.edu.ufersa.leon.leon.LeonApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -18,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage(LeonApplication.class.getPackageName()))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(buildApiInfo());
